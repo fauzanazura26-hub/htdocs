7ba75f83a216fcd036c21af78a1cccb855e7c0fb
fauzanazura26 <fauzanazura26@gmail.com>
2026-04-29 09:51:43 +0700

    pertemuan 8

--- 5026241161/routes/web.php
+++ 5026241161/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController ;
 
 Route::get('/', function () {
     return view('welcome');
@@ -48,3 +50,10 @@ Route::get('linktree', function () {
 Route::get('dosen', [DosenController::class, 'index']);
 Route::get('biodata', [DosenController::class, 'biodata']);
 
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+//blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
